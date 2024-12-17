4bd79d3de6618dc0881e938abaf7ab8b8ebcdb76
Jahnavi Vemuri <jvemuri@umd.edu>
2024-12-17 18:49:39 -0500

    Wrote a test to test if diceRoll.py is functioning as it should be

--- diceRoll.py
+++ diceRoll.py
@@ -3,3 +3,14 @@ import random
 
 def dice_roll():
     return [random.randint(1, 6) for _ in range(3)]
+
+# def test_dice_roll():
+#     random.seed(42)
+#     result = dice_roll()
+#     print(f"Dice rolled: {result}")
+
+#     # Dice roll should return exactly 3 values and all dice rolls should be between 1 and 6.
+#     assert len(result) == 3
+#     assert all(1 <= value <= 6 for value in result)
+
+# test_dice_roll()
